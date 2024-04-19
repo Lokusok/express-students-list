c0abff40fb2cd61f951ff33b8cad9cbd0cff9d2d
Lokusok <lokusokdrobk@gmail.com>
2024-04-19 20:14:48 +0300

    add login and register logic

--- controllers/session/index.ts
+++ controllers/session/index.ts
@@ -57,15 +57,86 @@ class SessionController {
   /**
    * Аутентификация пользователя
    */
-  async authenticate(req: Request, res: Response) {
-    res.send({ message: 'Under construct' });
+  async remind(req: Request, res: Response) {
+    // @ts-ignore
+    const sessionUserId = req.session.userId;
+
+    console.log('Authenticate reqSession: ', req.session);
+    if (sessionUserId) {
+      const findUser = await User.findOne({
+        where: {
+          id: sessionUserId,
+        },
+      });
+      const profileInfo = { ...findUser.dataValues };
+      delete profileInfo.password;
+      console.log({ profileInfo });
+
+      return res.status(200).send(profileInfo);
+    }
+
+    res.status(200).send(null);
   }
 
   /**
-   * Авторизация пользователя
+   * Логинизация пользователя
    */
-  async authorize(req: Request, res: Response) {
-    res.send({ message: 'Under construct' });
+  async login(req: Request, res: Response) {
+    try {
+      const { login, password } = req.body;
+
+      await new Promise((res) => setTimeout(res, 3000));
+
+      console.log({ login, password });
+
+      if (!login || !password) {
+        return res
+          .status(401)
+          .send({ error: 'Неверные данные для логинизации' });
+      }
+
+      const findUser = await User.findOne({
+        where: {
+          login,
+        },
+      });
+
+      if (findUser) {
+        console.log('FindUserPassword:', findUser.getDataValue('password'));
+        const isDataCorrect = bcrypt.compareSync(
+          password,
+          findUser.getDataValue('password')
+        );
+        console.log('Login User: ', findUser);
+        console.log({ isDataCorrect });
+
+        if (isDataCorrect) {
+          // @ts-ignore
+          req.session.userId = findUser.getDataValue('id');
+          return res.status(200).send(findUser);
+        }
+      }
+
+      res.status(401).send({ error: 'Неверные данные!' });
+    } catch (err) {
+      console.log('ERROR:', err);
+      res.status(400).send({ error: 'Ошибка при входе в аккаунт...' });
+    }
+  }
+
+  /**
+   * Удаление сессии пользователя
+   */
+  async logout(req: Request, res: Response) {
+    try {
+      console.log('Logout session:', req.session);
+      req.session.destroy((err) => {
+        console.log('Logout err:', err);
+      });
+      res.status(200).send(null);
+    } catch (err) {
+      res.status(400).send({ error: 'Ошибка при выходе из аккаунта...' });
+    }
   }
 }
 
--- routers/session/index.ts
+++ routers/session/index.ts
@@ -5,7 +5,8 @@ import { sessionController } from '../../controllers';
 const sessionRouter = Router();
 
 sessionRouter.post('/register', sessionController.register);
-sessionRouter.post('/authenticate', sessionController.authenticate);
-sessionRouter.post('/authorize', sessionController.authorize);
+sessionRouter.post('/remind', sessionController.remind);
+sessionRouter.post('/login', sessionController.login);
+sessionRouter.post('/logout', sessionController.logout);
 
 export default sessionRouter;
